810fa583049afed9768ce0b8e0b30171badf8b6f
sanal1989 <sanalsalynov@gmail.com>
2023-08-01 14:59:30 +0900

    Initial commit

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,17 @@
+package app.config;
+
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+    @Bean
+    public Timer getTimer(){
+        Timer timer = new Timer();
+        return timer;
+    }
+}
